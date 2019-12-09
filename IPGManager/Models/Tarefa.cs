6b10f16684bd91bbc3d0553e6bc7faab550dc9c7
Tiago Filipe da Costa Lourenço <tiago9l@hotmail.com>
2019-12-09 23:46:10 +0000

    cargosController tarefasmodel

---
+++ IPGManager/Models/Tarefa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace IPGManager.Models
+{
+    public class Tarefa
+    {
+        [Key]
+        public int TarefaId { get; set; }
+        [Required(ErrorMessage ="...")]
+        
+        public string NomeTarefa { get; set; }
+        public string DescricaoTarefa { get; set; }
+        public DateTime MyProperty { get; set; }
+
+
+
+
+    }
+}
