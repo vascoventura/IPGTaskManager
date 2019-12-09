4b0b2a90cb05a88e8a49d83f5ca6dbce1f155785
vascoventura <vasco.acv@hotmail.com>
2019-12-09 10:25:27 +0000

    CRUD Completa (Falta migracoes)

---
+++ IPGManager/Migrations/20191204170852_Initial.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IPGManager.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cargos",
+                columns: table => new
+                {
+                    IdCargo = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCargo = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cargos", x => x.IdCargo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departamentos",
+                columns: table => new
+                {
+                    IdDepartamento = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeDepartamento = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos", x => x.IdDepartamento);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Horarios",
+                columns: table => new
+                {
+                    IdHorario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HInicio = table.Column<DateTime>(nullable: false),
+                    HInicioIntervalo = table.Column<DateTime>(nullable: false),
+                    HFimIntervalo = table.Column<DateTime>(nullable: false),
+                    HFim = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Horarios", x => x.IdHorario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    IdFuncionarios = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: false),
+                    DataNascimento = table.Column<string>(nullable: false),
+                    Sexo = table.Column<string>(nullable: false),
+                    IdCargo = table.Column<int>(nullable: false),
+                    CargoIdCargo = table.Column<int>(nullable: true),
+                    IdDepartamento = table.Column<int>(nullable: false),
+                    IdHorario = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.IdFuncionarios);
+                    table.ForeignKey(
+                        name: "FK_Funcionarios_Cargos_CargoIdCargo",
+                        column: x => x.CargoIdCargo,
+                        principalTable: "Cargos",
+                        principalColumn: "IdCargo",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionarios_CargoIdCargo",
+                table: "Funcionarios",
+                column: "CargoIdCargo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departamentos");
+
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+
+            migrationBuilder.DropTable(
+                name: "Horarios");
+
+            migrationBuilder.DropTable(
+                name: "Cargos");
+        }
+    }
+}
---
+++ IPGManager/Models/SeedData.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPGManager.Models
+{
+    public class SeedData
+    {
+        private const string ADMIN_ROLE = "admin";
+        private const string MANAGER_ROLE = "manager";
+        private const string CHEF_ROLE = "chef";
+
+        public static void Populate(IPGManagerDBContext db)
+        {
+            // ...
+        }
+
+        public static async Task PopulateUsersAsync(UserManager<IdentityUser> userManager)
+        {
+            const string ADMIN_USERNAME = "admin@ipg.pt";
+            const string ADMIN_PASSWORD = "Secret123$";
+
+            const string MANAGER_USERNAME = "peter@ipg.pt";
+            const string MANAGER_PASSWORD = "Secret123$";
+
+            IdentityUser user = await userManager.FindByNameAsync(ADMIN_USERNAME);
+            if (user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = ADMIN_USERNAME,
+                    Email = ADMIN_USERNAME
+                };
+
+                await userManager.CreateAsync(user, ADMIN_PASSWORD);
+            }
+
+            if (!await userManager.IsInRoleAsync(user, ADMIN_ROLE))
+            {
+                await userManager.AddToRoleAsync(user, ADMIN_ROLE);
+            }
+
+            user = await userManager.FindByNameAsync(MANAGER_USERNAME);
+            if (user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = MANAGER_USERNAME,
+                    Email = MANAGER_USERNAME
+                };
+
+                await userManager.CreateAsync(user, MANAGER_PASSWORD);
+            }
+
+            if (!await userManager.IsInRoleAsync(user, MANAGER_ROLE))
+            {
+                await userManager.AddToRoleAsync(user, MANAGER_ROLE);
+            }
+
+            user = await userManager.FindByNameAsync("test@ipg.pt");
+            if (user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = "test@ipg.pt",
+                    Email = "test@ipg.pt"
+                };
+
+                await userManager.CreateAsync(user, ADMIN_PASSWORD);
+            }
+
+            // Create other user accounts ...
+        }
+
+        public static async Task CreateRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            //const string CAN_ADD_MENUS = "can_add_menus";
+
+            if (!await roleManager.RoleExistsAsync(ADMIN_ROLE))
+            {
+                await roleManager.CreateAsync(new IdentityRole(ADMIN_ROLE));
+            }
+
+            if (!await roleManager.RoleExistsAsync(MANAGER_ROLE))
+            {
+                await roleManager.CreateAsync(new IdentityRole(MANAGER_ROLE));
+            }
+
+            if (!await roleManager.RoleExistsAsync(CHEF_ROLE))
+            {
+                await roleManager.CreateAsync(new IdentityRole(MANAGER_ROLE));
+            }
+        }
+    }
+}
