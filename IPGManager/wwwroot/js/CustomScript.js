4d080fb3379b0a250ab9ebdfb04c6d246dce1baa
Tiago Filipe da Costa Lourenço <tiago9l@hotmail.com>
2020-01-23 02:45:03 +0000

    javascriptcoisinho

---
+++ IPGManager/wwwroot/js/CustomScript.js
@@ -0,0 +1,12 @@
+﻿function confirmDelete( isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' ;
+    var confirmDeleteSpan = 'confirmDeleteSpan_';
+
+    if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
